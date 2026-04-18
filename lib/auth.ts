71b0f0c49759ff58658b0f0ce2204059d81b90ae
Musbi <musbi700@gmail.com>
2026-04-18 21:22:34 +0000

    feat(auth): add trusted origins for better authentication handling

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,6 +5,10 @@ import prisma from "./prisma";
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
   secret: process.env.BETTER_AUTH_SECRET,
+  trustedOrigins: [
+    process.env.BETTER_AUTH_URL as string,
+    "http://localhost:3000",
+  ],
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
